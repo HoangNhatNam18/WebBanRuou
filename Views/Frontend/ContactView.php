c58b414da03cf8a98ad8c4bdde5c3c2f344ce78d
HoangNhatNam18 <namhoangnhat18@gmail.com>
2021-10-15 21:34:18 +0700

    upload code

---
+++ Views/Frontend/ContactView.php
@@ -0,0 +1,8 @@
+<?php 
+  //load file LayoutTrangChu.php
+  $this->fileLayout = "Layout_trang_trong.php";
+ ?>
+<div class="middle">
+    <h3>Liên hệ</h3>
+    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3723.808871871707!2d105.7722724496899!3d21.040332185923354!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x313454c9755033dd%3A0xf13b328d1266f239!2zMTQ3IFBo4buRIE1haSBE4buLY2gsIE1haSBE4buLY2gsIEPhuqd1IEdp4bqleSwgSMOgIE7hu5lpLCBWaeG7h3QgTmFt!5e0!3m2!1svi!2s!4v1630507253354!5m2!1svi!2s" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+</div>
